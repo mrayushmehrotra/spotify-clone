a5807532f15e30d6f6a4d1089babd34b27d379e9
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2023-11-30 20:05:17 +0530

    First commit

---
+++ components/Library.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+const Library = () => {
+  const onClick = () => {
+    //handle upload
+  };
+  return <div>Library</div>;
+};
+
+export default Library;
