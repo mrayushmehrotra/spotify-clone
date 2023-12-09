77ddb25e0897af88911b6c09712bdf07e62d1c78
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2023-12-09 17:48:11 +0530

    added liked song fn and search song

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+function useDebounce<T>(value: T, delay?: number): T {
+  const [debounceValue, setDebounceValue] = useState<T>(value);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay || 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+  return debounceValue;
+}
+
+export default useDebounce;
