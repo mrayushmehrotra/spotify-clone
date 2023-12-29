e01479f80d348a0c2d719481c6d4a90a2e7a59a6
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2023-12-29 21:07:55 +0530

    added not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Box from "@/components/Box";
+import Button from "@/components/Button";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const NotFound = () => {
+  const router = useRouter();
+  return (
+    <>
+      <Box className="flex flex-col items-center justify-center w-full h-full">
+        <p>Go where songs let you go</p>
+        <div className="m-5"></div>
+        <Button onClick={() => router.push("/")}>
+          <h1>Home</h1>
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export default NotFound;
