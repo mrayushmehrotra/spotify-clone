53cd3c6d177dccb828c692b2211acd310af55b01
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2023-12-29 21:18:27 +0530

    added vercel insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,29 @@
-import { Figtree } from 'next/font/google'
+import { Figtree } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import getSongsByUserId from "@/actions/getSongsByUserId";
+import getActiveProductsWithPrices from "@/actions/getActiveProductsWithPrices";
+import Sidebar from "@/components/Sidebar";
+import ToasterProvider from "@/providers/ToasterProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import Player from "@/components/Player";
 
-import getSongsByUserId from '@/actions/getSongsByUserId'
-import getActiveProductsWithPrices from '@/actions/getActiveProductsWithPrices'
-import Sidebar from '@/components/Sidebar'
-import ToasterProvider from '@/providers/ToasterProvider'
-import UserProvider from '@/providers/UserProvider'
-import ModalProvider from '@/providers/ModalProvider'
-import SupabaseProvider from '@/providers/SupabaseProvider'
-import Player from '@/components/Player'
+import "./globals.css";
 
-import './globals.css'
-
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Spotify Clone',
-  description: 'Spotify Clone',
-}
+  title: "Spotify - Music Streaming Platform",
+  description: "Spotify hear what your heart wants.",
+};
 
 export const revalidate = 0;
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   const products = await getActiveProductsWithPrices();
   const userSongs = await getSongsByUserId();
@@ -35,13 +35,12 @@ export default async function RootLayout({
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider products={products} />
-            <Sidebar songs={userSongs}>
-              {children}
-            </Sidebar>
+            <Sidebar songs={userSongs}>{children}</Sidebar>
             <Player />
           </UserProvider>
         </SupabaseProvider>
+        <SpeedInsights />
       </body>
     </html>
-  )
+  );
 }
