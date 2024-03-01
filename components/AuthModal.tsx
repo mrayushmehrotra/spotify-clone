03ca061742bf2fb40a308502a26e2948dc48c2ea
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2024-02-29 22:41:05 -0800

    added demo email pass

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,23 +1,24 @@
 "use client";
 
-import React, { useEffect } from 'react';
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
-import { 
-  useSessionContext, 
-  useSupabaseClient
-} from '@supabase/auth-helpers-react';
-import { useRouter } from 'next/navigation';
+import React, { useEffect } from "react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import {
+  useSessionContext,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
 
 import useAuthModal from "@/hooks/useAuthModal";
 
-import Modal from './Modal';
+import Modal from "./Modal";
+import toast from "react-hot-toast";
 
 const AuthModal = () => {
   const { session } = useSessionContext();
   const router = useRouter();
   const { onClose, isOpen } = useAuthModal();
-  
+
   const supabaseClient = useSupabaseClient();
 
   useEffect(() => {
@@ -31,34 +32,42 @@ const AuthModal = () => {
     if (!open) {
       onClose();
     }
-  }
+  };
 
   return (
-    <Modal 
-      title="Welcome back" 
-      description="Login to your account." 
-      isOpen={isOpen} 
-      onChange={onChange} 
+    <Modal
+      title="Welcome back"
+      description="Login to your account."
+      isOpen={isOpen}
+      onChange={onChange}
     >
+      <div>
+        <h1>
+          <span>Email:= </span>demo@gmail.com
+        </h1>
+        <h1>
+          <span>Password:= </span>demodemo
+        </h1>
+      </div>
       <Auth
         supabaseClient={supabaseClient}
-        providers={['github']}
+        providers={["github"]}
         magicLink={true}
         appearance={{
           theme: ThemeSupa,
           variables: {
             default: {
               colors: {
-                brand: '#404040',
-                brandAccent: '#22c55e'
-              }
-            }
-          }
+                brand: "#404040",
+                brandAccent: "#22c55e",
+              },
+            },
+          },
         }}
         theme="dark"
       />
     </Modal>
   );
-}
+};
 
 export default AuthModal;
